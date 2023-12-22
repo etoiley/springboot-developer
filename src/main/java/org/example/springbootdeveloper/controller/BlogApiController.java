2517cdd43d12eb9b56d887e6680df460b5e998b4
etoiley <etoile13y@gmail.com>
2023-12-23 00:30:20 +0900

    Blog api2

---
+++ src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,30 @@
+package org.example.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
+import org.example.springbootdeveloper.dto.AddArticleRequest;
+import org.example.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController //HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    //HTTP 메소드가 POST일 때 전달받은 URL과 동일하면 메소드로 매핑
+    @PostMapping("/api/articles")
+    //@RequestBody로 요청 본문 값 매핑
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        //요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+        //ResponseEntity.status().body()는 응답코드로 201. 즉 Created를 응답하고 테이블에 저장된 객체를 반환
+
+    }
+}
