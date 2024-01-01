081eee00ef3329945c319db0d908e6b62eee2220
etoiley <100402409+etoiley@users.noreply.github.com>
2024-01-01 23:05:37 +0900

    thymeleaf 삭제/수정

--- src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
@@ -7,8 +7,10 @@ import org.example.springbootdeveloper.dto.ArticleViewResponse;
 import org.example.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 @RequiredArgsConstructor
@@ -35,4 +37,19 @@ public class BlogViewController {
 
         return "article";
     }
+
+    @GetMapping("/new-article")
+    //id키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false, name = "id")Long id, Model model){
+        if(id == null){// id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        }else{// id가 없으면 생성
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
+
+    }
+
+
 }
---
+++ src/main/resources/static/article.js
@@ -0,0 +1,43 @@
+//삭제 기능
+const deleteButton = document.getElementById('delete-btn');
+
+if(deleteButton){
+    deleteButton.addEventListener('click', event => {
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/articles/${id}`, {
+            method : 'DELETE'
+        })
+            .then(() => {
+                alert("삭제가 완료되었습니다.");
+                location.replace('/articles');
+                //location.replace() 메소드는 실행 시 사용자의 웹 브라우저 화면을 현재 주소를 기반해 옮겨주는 역할을 한다.
+            });
+    });
+}
+
+//수정 기능
+//id가 modify-btn인 엘리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton){
+    //클릭 이벤트가 감지되면 수정 API 요청
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method : 'PUT',
+            headers : {
+                "Content-type" : "application/json",
+            },
+            body : JSON.stringify({
+                title : document.getElementById('title').value,
+                content : document.getElementById('content').value
+            })
+        })
+            .then(() => {
+                alert("수정이 완료되었습니다.");
+                location.replace(`/articles/${id}`)
+            });
+    });
+}
